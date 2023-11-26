f71100a787cd41aef95a18a4822e2e3ca7a3627e
Spacerulerwill <williamdredding@gmail.com>
2023-11-26 19:33:07 +0000

    Better comments, code formatting

--- life.c
+++ life.c
@@ -7,27 +7,63 @@
 
 int main(int argc, char *argv[])
 {
+    /*
+    Program only takes 2 arguments, the width and height of the grid
+    so if we receive any number other than these 3 arguments:
+    * Name of program (implicitly passed)
+    * Width (int)
+    * Height (int)
+    */ 
     if (argc != 3) {
+        fputs("Invalid number of arguments passed! Program usage: ./life (width 1 - 255) (height 1 - 255)\n", stderr);
         return EXIT_FAILURE;
     }
 
-    uintmax_t width = strtoumax(argv[1], NULL, 10);
-    if ((width == UINTMAX_MAX && errno == ERANGE) || width == 0) {
-        return EXIT_FAILURE;
+    // Width and height of the playable area in game of life
+    uint8_t width;
+    uint8_t height;
+
+    char* endptr;
+    {
+        int lwidth = strtol(argv[1], &endptr, 10);
+        if (*endptr != '\0') {
+            fputs("Value provided for argument 1 - width - is not a valid integer\n", stderr);
+            return EXIT_FAILURE;
+        }
+        if (lwidth < 1 || lwidth > 255) {
+            fputs("Value provided for argument 1 - width - is not in the range 1 - 255\n", stderr);
+            return EXIT_FAILURE;
+        }
+        width = (uint8_t)lwidth;
     }
 
-    uintmax_t height = strtoumax(argv[2], NULL, 10);
-    if ((height == UINTMAX_MAX && errno == ERANGE) || height == 0) {
-        return EXIT_FAILURE;
+    {
+        int lheight = strtol(argv[2], &endptr, 10);
+        if (*endptr != '\0') {
+            fputs("Value provided for argument 2 - height - is not a valid integer\n", stderr);
+            return EXIT_FAILURE;
+        }
+
+        if (lheight < 1 || lheight > 255) {
+            fputs("Value provided for argument 2 - height - is not in the range 1 - 255\n", stderr);
+            return EXIT_FAILURE;
+        }
+
+        height = (uint8_t)lheight;
     }
 
+    /*
+    The char buffer needs to store an extra column on the left for a border
+    and 2 extra column on the right for a border and then newline chars.
+    There must also be an extra row on the top and bottom for borders above
+    and below.
+    */
     uintmax_t totalChars = (width + 3) * (height + 2);
 
     char* buffer = (char*)malloc(sizeof(char) * ((width + 3) * (height + 2)));
     memset(buffer, ' ', sizeof(char) * totalChars);
 
-    // Create grid in char buffer
-    // top line
+    // Create top border
     int idx = 0;
     buffer[idx] = '+';
     for (++idx; idx < width + 1; idx++) {
@@ -36,7 +72,7 @@ int main(int argc, char *argv[])
     buffer[idx++] = '+';
     buffer[idx++] = '\n';
 
-    // middle lines
+    // For each middle row, add a border char to each side and a newline on the right
     idx = width + 3;
     for (; idx < totalChars; idx += width + 3) {
         buffer[idx] = '|';
@@ -44,25 +80,33 @@ int main(int argc, char *argv[])
         buffer[idx + width + 2] = '\n';
     }
 
-    // bottom line
+    // Create bottom border
     idx = ((width + 3) * (height + 1));
     buffer[idx] = '+';
     for (++idx; idx < (width + 3) * (height + 1) + width + 1; idx++) {
         buffer[idx] = '-';
     }
     buffer[idx++] = '+';
-
+    
+    // Init curses screen
     initscr();
+
+    // Draw our char buffer and refresh the screen
     addstr(buffer);
     refresh();
-    int inputCode;
+
+    // Our initial mouse position is (1,1) as to not be on the border
     int x = 1;
     int y = 1;
     move(y, x);
+
+    // Ncurses input settings
     raw(); 
     keypad(stdscr, TRUE); 
     intrflush(stdscr, FALSE);
     noecho();
+
+    int inputCode;
     do {
         inputCode = getch();
 
@@ -105,6 +149,8 @@ int main(int argc, char *argv[])
             }
         }
     } while (inputCode != 'q');
+
+    // Cleanup
     endwin();
     free(buffer);
     return EXIT_SUCCESS;
